2279f1cb61b79aef3d74c1a80109f9a3c86f6806
晓春 薛 <2467315534@qq.com>
2025-01-22 00:47:36 +0800

    Stop tracking build directory

--- build/.qt/QtDeployTargets-MinSizeRel.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MyIDE_FILE D:/mycppfiles/QT/MyIDE/build/MinSizeRel/MyIDE.exe)
-set(__QT_DEPLOY_TARGET_MyIDE_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MyIDE_RUNTIME_DLLS D:/QT/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/QT/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/QT/6.8.0/mingw_64/bin/Qt6Core.dll)
